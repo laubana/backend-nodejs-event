d8273caf412dc0e81d03e67523113ab8b9aea7c1
yban <laubana@gmail.com>
2025-01-30 21:56:09 -0800

    add vercel

--- index.mjs
+++ index.mjs
@@ -2,6 +2,8 @@ import cors from "cors";
 import { config } from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 import corsOptions from "./configs/cors.mjs";
 import connect from "./configs/db.mjs";
@@ -14,6 +16,10 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use("/", express.static(join(__dirname, "public")));
 app.use("/api", event);
 
 mongoose.connection.once("open", () => {
